bde6bea402c61ab1aa0ad0fb2191d2d81fd7e6fd
Lilith Tundrus <echofox123@gmail.com>
2018-09-01 22:43:41 -0400

    Fucking with oauth stuff now... this is really challenging

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,13 @@ import ReditFetchClient from './FetchClient';
 
 // Options for the reddit client, contains the API plus subreddit config options
 // TODO: add typings for this
-const configOptions = require('../config.json');
+const configOptions = fs.readFileSync('../config.json').toString();
+const parsedConfigOptions = JSON.parse(configOptions);
 
 let client = new ReditFetchClient('reddit.com/', '');
 
-client.getSubreddit('Test')
+let authorizationURL: string = `${parsedConfigOptions.redditBaseURL}`;
+
+let testURL = `https://www.reddit.com/api/v1/authorize?client_id=${parsedConfigOptions.redditClientID}&response_type=code&state=1234&redirect_uri=${parsedConfigOptions.redditRedirectURL}&duration=permanent&scope=read`;
+
+client.getSubreddit(testURL)
