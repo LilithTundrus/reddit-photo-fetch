00a4aac28899306e212b2e65c7f8e83e18325152
Lilith Tundrus <echofox123@gmail.com>
2018-09-03 01:26:18 -0400

    Removed old code form refreshToken.ts. Hopefully this file is no longer needed after using snoowrap

--- src/refreshToken.ts
+++ src/refreshToken.ts
@@ -4,23 +4,6 @@
 // Use request for POSTing to reddit
 import * as request from 'request';
 
-
-// OLD CODE
-
-// let requestOptions = {
-//     headers: {
-//         'Authorization': `Basic ${encoding}`
-//     },
-//     url: 'https://www.reddit.com/api/v1/access_token',
-//     body: `grant_type=authorization_code&code=&redirect_uri=`
-// };
-
-// // Initially getting the token
-// request.post(requestOptions, (resposne, err) => {
-//     console.log(resposne, err)
-// });
-
-
 export function refreshToken(configJSON: any, accessToken: string, refreshToken: string, cb: any) {
 
     // Make sure the query params/headers are set correctly
