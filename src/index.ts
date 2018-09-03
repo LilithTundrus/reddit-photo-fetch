7a2f7b68dbffe6ef1eb3b567af6532c8d37e3a76
Lilith Tundrus <echofox123@gmail.com>
2018-09-02 21:30:39 -0400

    example is now working

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,34 @@
 
 // Node/NPM requires and imports
 import * as fs from 'fs';
-import ReditFetchClient from './FetchClient';
+// Dirty import of the Reddit API wrapper because its typings aren't correct
+const snoowrap = require('snoowrap');
+import { refreshToken } from './refreshToken';
 
 // Options for the reddit client, contains the API plus subreddit config options
 // TODO: add typings for this
 const configOptions = fs.readFileSync('../config.json').toString();
 const parsedConfigOptions = JSON.parse(configOptions);
 
-// TODO: just use snoo instead of using our own thing
+let wrapper = new snoowrap({
+    userAgent: parsedConfigOptions.redditUserAgent,
+    clientId: parsedConfigOptions.redditClientID,
+    clientSecret: parsedConfigOptions.redditSecret,
+    refreshToken: parsedConfigOptions.redditRefreshToken
+});
 
-let client = new ReditFetchClient('reddit.com/', '');
+// Printing a list of the titles on the front page
+wrapper.getSubreddit('ArousingAvians').getNew().map(post => post.title).then(console.log);
 
-let authorizationURL: string = `${parsedConfigOptions.redditBaseURL}`;
 
-let testURL = `https://www.reddit.com/api/v1/authorize?client_id=${parsedConfigOptions.redditClientID}&response_type=code&state=1234&redirect_uri=${parsedConfigOptions.redditRedirectURL}&duration=permanent&scope=read`;
+// refreshToken(parsedConfigOptions, null, parsedConfigOptions.redditRefreshToken, (data) => {
+    // Refresh the token using the information
 
-client.getSubreddit(testURL)
+//     console.log(data)
+//     console.log(2)
+// })
+
+
+// Write the new token to file
+
+// Do what we need here
