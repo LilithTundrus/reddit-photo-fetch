2b7ed253befbc2c19b27a68e4111e37e4f60fcbb
Lilith Tundrus <echofox123@gmail.com>
2018-09-17 21:43:20 -0400

    Renamed testing file, added a single test

---
+++ src/test.ts
@@ -0,0 +1,19 @@
+// Using ES5 strict mode
+'use strict';
+
+//  Imports
+
+import ReditFetchClient from './FetchClient';
+
+
+
+// These are Jest based tests, but could probably work in most testing frameworks
+
+// Unit tests
+
+test('FetchClient Should NOT work without an error', () => {
+    expect(new ReditFetchClient(null, null, null, null)).toThrowError();
+})
+
+
+// Download tests here
