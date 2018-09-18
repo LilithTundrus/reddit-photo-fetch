9d9dfcbb3eb0f7533fbf47a86525867bec829536
Lilith Tundrus <echofox123@gmail.com>
2018-09-17 21:53:18 -0400

    Min or changes to test file

--- src/test.ts
+++ src/test.ts
@@ -5,15 +5,13 @@
 
 import ReditFetchClient from './FetchClient';
 
-
-
 // These are Jest based tests, but could probably work in most testing frameworks
 
 // Unit tests
 
 test('FetchClient Should NOT work without an error', () => {
-    expect(new ReditFetchClient(null, null, null, null)).toThrowError();
+    expect(new ReditFetchClient(null, null, null, null)).toThrow(Error);
 })
 
 
-// Download tests here
+// Download tests
