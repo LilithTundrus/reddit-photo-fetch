af8788398d93da1b050c5f7d1da274ad3bc090e7
Lilith Tundrus <echofox123@gmail.com>
2020-10-03 00:18:20 -0400

    Dude fuck bad typings

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,7 @@ let rfc: ReditFetchClient;
 // TODO: re-add the filename checking, make sure before an image is downloaded that it does
 // not already exist with the same file size
 // TODO: create a scheduling system for this script
+// TODO: Support FA page scrapes
 
 // If there's an argument present (ignoring the first 2 indeces of `node` and `index.js`)
 if (process.argv[2]) {
